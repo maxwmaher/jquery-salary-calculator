0182a562a91c8c1cc5f6280403803ae12f169417
Max Maher <maxwmaher@gmail.com>
2019-07-27 10:03:49 -0500

    able to enter employee information to table

--- client.js
+++ client.js
@@ -3,5 +3,24 @@ console.log('Hello World');
 $(document).ready(readyNow);
 
 function readyNow() {
+    $('#submit-button').on('click', submitEmployee);
+}
 
+function submitEmployee() {
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let idNumber = $('#id-number').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annual-salary').val();
+    console.log(firstName);
+    
+    $('.tbody').append(`
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${idNumber}</td>
+        <td>${title}</td>
+        <td>$${annualSalary}</td>
+    </tr>
+    `)
 }
